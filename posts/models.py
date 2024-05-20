2036d330a115918cb59a9961450336be4b500543
hiefts <101970576+hiefts@users.noreply.github.com>
2024-05-20 22:48:12 +0900

    login page

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 
 # Create your models here.
+def get_default_superuser_id():
+    User = get_user_model()
+    superuser = User.objects.filter(is_superuser=True).first()
+    return superuser.id if superuser else None
+
 class Post(models.Model):
   title = models.CharField(max_length=100)
   date = models.DateTimeField(auto_now_add=True)
   description = models.CharField(max_length=100)
   content = models.TextField()
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  user = models.ForeignKey(
+      get_user_model(),
+      on_delete=models.CASCADE,
+      default=get_default_superuser_id
+    )
 
   def __str__(self):
       return self.title
